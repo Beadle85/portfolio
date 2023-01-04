cf067a128e7b1e62e495807b11688e61b4b57e93
Beadle <mathub85@icloud.com>
2023-01-04 15:20:42 -0600

    start creating layout design

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,11 @@
+import styles from './layout.module.css';
+
 export default function Layout() {
-  return <div>I will be everywhere. I will be your navbar</div>;
+  return (
+    <nav className={styles.container}>
+      <a href='#home'>Home</a>
+      <a href='#about'>About</a>
+      <a href='#contact'>Contact</a>
+    </nav>
+  );
 }
