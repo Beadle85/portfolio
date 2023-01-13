be98ea1c887f0456cfa0a258a53723e3b49b3d8e
Beadle <mathub85@icloud.com>
2023-01-12 20:27:58 -0600

    adjust approach to make the layout and design simpler

--- components/home.tsx
+++ components/home.tsx
@@ -1,16 +1,13 @@
 import Image from 'next/image';
 import styles from './home.module.css';
 import profilePic from '../public/images/profile.jpg';
-import FunctionCard from './home/functionCard';
 
 export default function Home() {
   return (
     <section id='home' className={styles.container}>
-      <div className={styles.backgroundDiv} />
-      <div>
-        <Image src={profilePic} alt='picture of Beadle' width={456} height={456} />
-      </div>
-      <FunctionCard />
+      <h1 className={styles.title}>Full Stack Software Engineer</h1>
+      <h3 className={styles.subTitle}>I enjoy making companies more productive and efficient through code</h3>
+      <Image src={profilePic} alt='picture of Beadle' width={456} height={456} />
     </section>
   );
 }
