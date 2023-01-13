98d2027f1863052ed35618fe207e0f93b7ee2422
Beadle <mathub85@icloud.com>
2023-01-12 20:30:59 -0600

    add back function card

--- components/home.tsx
+++ components/home.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import styles from './home.module.css';
 import profilePic from '../public/images/profile.jpg';
+import FunctionCard from './home/functionCard';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <h1 className={styles.title}>Full Stack Software Engineer</h1>
       <h3 className={styles.subTitle}>I enjoy making companies more productive and efficient through code</h3>
       <Image src={profilePic} alt='picture of Beadle' width={456} height={456} />
+      <FunctionCard />
     </section>
   );
 }
